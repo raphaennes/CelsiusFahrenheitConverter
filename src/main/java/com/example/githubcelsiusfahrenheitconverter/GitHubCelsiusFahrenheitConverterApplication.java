ee105b9b123c8b9fbb626c35e83202065a6b7572
raphaennes <raphaelneres43@gmail.com>
2025-11-21 11:46:56 -0300

    Initial commit

---
+++ src/main/java/com/example/githubcelsiusfahrenheitconverter/GitHubCelsiusFahrenheitConverterApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubcelsiusfahrenheitconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubCelsiusFahrenheitConverterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitHubCelsiusFahrenheitConverterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/githubcelsiusfahrenheitconverter/GitHubCelsiusFahrenheitConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubcelsiusfahrenheitconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubCelsiusFahrenheitConverterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
